db2d5783bf0fcdd2dd0b4ea3dd78cf5e10f46b84
Ander Uraga <ander.uraga@gmail.com>
2022-10-13 10:09:28 +0200

    terminado insert persona

--- src/main/java/com/txurdi/persistencia/crud/basico/InsertarPersona.java
+++ src/main/java/com/txurdi/persistencia/crud/basico/InsertarPersona.java
@@ -7,9 +7,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+
 /**
  * Ejemplo básico para crear una nueva persona<br>
  * id es autoincremental, no hace falta solicitarlo <br>
+ * Cuidado el NIF debes er único, gestionamos la excepción <br>
  * 
  * @author Ander Uraga Real
  *
@@ -22,13 +25,19 @@ public class InsertarPersona {
 
 		// recursos autoclosables
 		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/txurdi?useSSL=false", "root",
-				"root"); PreparedStatement pst = con.prepareStatement(SQL); Scanner sc = new Scanner(System.in);) {
+				"root"); 
+				PreparedStatement pst = con.prepareStatement(SQL, PreparedStatement.RETURN_GENERATED_KEYS );
+				Scanner sc = new Scanner(System.in);) {
 
-			// solicitar datos			
-			// TODO harcoded data
-			String nombre = "dummy2";
-			String nif = "1111132H";
-			int edad = 44;
+			// solicitar datos
+			System.out.println("Nombre:");
+			String nombre = sc.nextLine();
+			
+			System.out.println("Nif:");
+			String nif = sc.nextLine();
+			
+			System.out.println("Edad [0-150]");
+			int edad = Integer.parseInt(sc.nextLine());
 
 			// sustituimmos intertogantes por valores
 			pst.setString(1, nombre);
@@ -40,10 +49,26 @@ public class InsertarPersona {
 
 			String msj = (affectedRows == 1) ? "Persona insertada" : "No se realizo la insert";
 			System.out.println(msj);
-
-			//TODO gestionar Duplicate entry '1111111H' for key 'persona.nif_UNIQUE'
 			
-		} // 1ºtry
+			//recuperar id autogenerado
+			if (affectedRows == 1) {
+				try( ResultSet rs = pst.getGeneratedKeys() ){
+					if( rs.next() ) {
+						int id = rs.getInt(1);
+						System.out.println("El ID generado de la persona es " + id);
+					}
+				}
+				
+			}
+			
+			
+
+		} catch (MySQLIntegrityConstraintViolationException e) {
+			System.out.println("NIF duplicado !!!! ");
+		
+		} catch (Exception e) {
+			System.out.println("Excepcion " + e.getMessage());
+		}
 
 	}
 
