c528e4b5b45d37659933fdcd51fcd5875f8d21f3
Ander Uraga <ander.uraga@gmail.com>
2022-10-20 11:04:30 +0200

    TODO terminar PersonaDAO

--- src/main/java/com/txurdi/persistencia/modelo/dao/PersonaDAO.java
+++ src/main/java/com/txurdi/persistencia/modelo/dao/PersonaDAO.java
@@ -12,16 +12,18 @@ import com.txurdi.persistencia.modelo.pojo.Persona;
 public class PersonaDAO implements Singleton, Crudable<Persona> {
 
 	private static PersonaDAO INSTANCE = null;
-	
+
 	// Sentencias SQL
 	private static final String SQL_FIND_ALL = "SELECT id, nombre, nif, edad FROM persona ORDER BY nombre ASC LIMIT 500; ";
-	
-	
+	private static final String SQL_FIN_BY_ID = "SELECT id, nombre, nif, edad FROM persona WHERE id = ? ; ";
+	private static final String SQL_UPDATE = "UPDATE `persona` SET `nombre` = ?, `nif` = ?, `edad` = ? WHERE `id` = ? ; ";
+	private static final String SQL_INSERT = "INSERT INTO `persona` (`nombre`, `nif`, `edad`) VALUES (?,?,?) ; ";
+	private static final String SQL_DELETE = "DELETE FROM `persona` WHERE `id` = ? ; ";
 
 	private PersonaDAO() {
 		super();
 	}
-	
+
 	public static PersonaDAO getInstance() {
 		if (INSTANCE == null) {
 			INSTANCE = new PersonaDAO();
@@ -32,25 +34,24 @@ public class PersonaDAO implements Singleton, Crudable<Persona> {
 	@Override
 	public List<Persona> findAll() {
 
-		ArrayList<Persona> regitros = new ArrayList<Persona>();		
+		ArrayList<Persona> regitros = new ArrayList<Persona>();
 
 		try (Connection con = ConnectionManager.open();
-			 PreparedStatement pst = con.prepareStatement(SQL_FIND_ALL);
-		     ResultSet rs = pst.executeQuery();
-		){
+				PreparedStatement pst = con.prepareStatement(SQL_FIND_ALL);
+				ResultSet rs = pst.executeQuery();) {
 
 			while (rs.next()) {
-				
+
 				Persona p = new Persona();
-				p.setId( rs.getInt("id"));
+				p.setId(rs.getInt("id"));
 				p.setNombre(rs.getNString("nombre"));
 				p.setNif(rs.getString("nif"));
-				// TODO resto de campos
-				
+				p.setEdad(rs.getInt("edad"));
+				// TODO mapper
+
 				regitros.add(p);
 			}
-			
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -61,26 +62,98 @@ public class PersonaDAO implements Singleton, Crudable<Persona> {
 
 	@Override
 	public Persona findById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Persona registro = null;
+
+		try (Connection con = ConnectionManager.open(); PreparedStatement pst = con.prepareStatement(SQL_FIN_BY_ID);
+
+		) {
+
+			pst.setInt(1, id);
+
+			try (ResultSet rs = pst.executeQuery()) {
+				while (rs.next()) {
+
+					registro = new Persona();
+					registro.setId(rs.getInt("id"));
+					registro.setNombre(rs.getNString("nombre"));
+					registro.setNif(rs.getString("nif"));
+					registro.setEdad(rs.getInt("edad"));
+					// TODO mapper
+
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return registro;
 	}
 
 	@Override
 	public void update(Persona pojo) throws Exception {
-		// TODO Auto-generated method stub
+
+		try (Connection con = ConnectionManager.open(); PreparedStatement pst = con.prepareStatement(SQL_UPDATE);
+
+		) {
+
+			// sustituimmos intertogantes por valores
+			pst.setString(1, pojo.getNombre());
+			pst.setString(2, pojo.getNif());
+			pst.setInt(3, pojo.getEdad());
+			pst.setInt(4, pojo.getId());
+
+			pst.executeUpdate();
+
+		}
 
 	}
 
 	@Override
 	public void insert(Persona pojo) throws Exception {
-		// TODO Auto-generated method stub
+
+		try (Connection con = ConnectionManager.open(); 
+			 PreparedStatement pst = con.prepareStatement(SQL_INSERT, PreparedStatement.RETURN_GENERATED_KEYS );
+			) {
+
+			// sustituimmos intertogantes por valores
+			pst.setString(1, pojo.getNombre());
+			pst.setString(2, pojo.getNif());
+			pst.setInt(3, pojo.getEdad());
+
+			// ejecutar pst y nos retorna un int == affetedRows
+			int affectedRows = pst.executeUpdate();
+
+			// recuperar id autogenerado
+			if (affectedRows == 1) {
+				try (ResultSet rs = pst.getGeneratedKeys()) {
+					if (rs.next()) {
+						int id = rs.getInt(1);
+						pojo.setId(id);
+					}
+				}
+
+			}//if
+
+		}//try
 
 	}
 
 	@Override
 	public boolean delete(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		boolean eliminado = false;
+		
+		try (Connection con = ConnectionManager.open(); PreparedStatement pst = con.prepareStatement(SQL_DELETE);) {
+			
+			pst.setInt(1, id);
+			int affectedRows = pst.executeUpdate();
+			if ( affectedRows == 1 ) {
+				eliminado = true;
+			}			
+		}
+		
+		return eliminado;
 	}
 
 }
